5701cf9d1667a9fa761bea2069797a60a44a57cc
joaovitor2614 <joaovitoralestrella@gmail.com>
2021-07-15 13:31:23 -0300

    first project

--- madlibs.py
+++
@@ -1,6 +0,0 @@
-name = input('Name: ')
-object = input('Object: ')
-pastVerb = input('Past verb: ')
-direction = input('Direction: ')
-
-print(f"The guy named {name} was scared becasue he saw a {object} be {pastVerb} on his {direction}")
